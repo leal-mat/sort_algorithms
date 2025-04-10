29fb0223ffccac5e59526670fddbe72651fb6fc2
mleal <mateuslg2000@gmail.com>
2025-04-09 21:07:24 -0300

    Initializing repo

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "sorting_algorithms/quicksort.h"
+
+int main()
+{
+    std::cout << "Sorting Algorithms" << std::endl;
+    return 0;
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Quick Sort" << std::endl;
+    quicksort();
+}
---
+++ sorting_algorithms/quicksort.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void quicksort()
+{
+    return;
+}
